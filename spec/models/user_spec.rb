e09651e396ed7dcdafd128a3046ef19a3b09035e
muggle95 <muggle95@users.noreply.github.com>
2014-09-21 16:26:50 +0000

    original user testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,78 +1,38 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe User do
-    let(:user) { User.create(name: 'John Doe', email: 'jdoe@example.com', password: 'password') }
-    subject { user }
+RSpec.describe User, :type => :model do
+	let(:user) {User.new name: "Katie", email: "kat.thomp1@gmail.com", password: "M3l4n!3"}
+	subject {user}
+	it {should respond_to(:name)}
+	it {should respond_to(:email)}
+	it {should respond_to(:password)}
 
-    it { should respond_to(:name) }
-    it { should respond_to(:email) }
-    it { should respond_to(:password) }
-
-    it { should be_valid }
-
-    describe "empty name" do
-	before { user.name = '' }
-
-	it { should_not be_valid }
-    end
-
-    describe "blank name" do
-	before { user.name = ' ' }
-
-	it { should_not be_valid }
-    end
-
-    describe "empty email" do
-	before { user.email = '' }
-
-	it { should_not be_valid }
-    end
-
-    describe "blank email" do
-	before { user.email = ' ' }
-
-	it { should_not be_valid }
-    end
-
-    describe "empty password" do
-	before { user.password = '' }
-
-	it { should_not be_valid }
-    end
-
-    describe "blank password" do
-	before { user.password = ' ' }
-
-	it { should_not be_valid }
-    end
-
-    describe "long name" do
-	before { user.name = 'a' * 51 }
-
-	it { should_not be_valid }
-    end
-
-    describe "duplicate name" do
-	let(:duplicate) do
-	    d = user.dup
-	    d.email = 'duplicate@example.com'
-	    d
+	it {should be_valid}
+	
+	describe "empty name" do
+		before {user.name=""}
+		it {should_not be_valid }
+	end
+	describe "blank name" do
+		before {user.name=" "}
+		it {should_not be_valid }
 	end
 
-	it "is not allowed" do
-	    expect(duplicate).not_to be_valid
+	describe "empty password" do
+		before {user.password=""}
+		it {should_not be_valid}
 	end
-    end
-
-    describe "duplicate email" do
-	let(:duplicate) do
-	    d = user.dup
-	    d.name = 'Jane Doe'
-	    d
+	describe "blank password" do
+		before {user.password= " "}
+		it {should_not be_valid}
 	end
 
-	it "is not allowed" do
-	    expect(duplicate).not_to be_valid
+	describe "empty email" do
+		before {user.email=""}
+		it {should_not be_valid}
 	end
-    end
+	describe "blank email" do
+		before {user.email = " "}
+		it {should_not be_valid}
+	end
 end
